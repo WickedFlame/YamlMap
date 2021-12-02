d68db870bc865593f36cdd987def1d1dfb77d8ec
Christian Walpen <christianwalpen@opacc.ch>
2021-12-02 09:39:03 +0100

    YamlFileWriter

---
+++ src/Tests/YamlMap.Tests/YamlFileWriterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+using Polaroider;
+
+namespace YamlMap.Tests
+{
+    public class YamlFileWriterTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var path = Path.Combine(Path.GetDirectoryName(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path)), "WriterTest.yml");
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+
+        [Test]
+        public void YamlFileWriter_Write()
+        {
+            var item = new YamlRoot
+            {
+                Id = "writer test"
+            };
+
+            var path = Path.Combine(Path.GetDirectoryName(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path)), "WriterTest.yml");
+
+            var writer = new YamlFileWriter();
+            writer.Write(path, item);
+
+            File.ReadAllText(path).MatchSnapshot();
+        }
+    }
+}
--- src/YamlMap/YamlFileWriter.cs
+++ src/YamlMap/YamlFileWriter.cs
@@ -1,22 +1,33 @@
 ﻿using System;
+using System.IO;
 
 namespace YamlMap
 {
-    //public class YamlFileWriter
-    //{
-    //    public string Write<T>(string file, T item)
-    //    {
-    //        var serialized = Serializer.Serialize(item);
+    /// <summary>
+    /// Write a yaml to a file
+    /// </summary>
+    public class YamlFileWriter
+    {
+        /// <summary>
+        /// Serializes a object to yaml and saves the yaml in a file
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public string Write<T>(string fileName, T item)
+        {
+            var serialized = Serializer.Serialize(item);
 
-    //        // save to file here
-    //        WriteFile(file, serialized);
+            // save to file here
+            WriteFile(fileName, serialized);
 
-    //        return serialized;
-    //    }
+            return serialized;
+        }
 
-    //    private void WriteFile(string file, string yaml)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //}
+        private void WriteFile(string fileName, string yaml)
+        {
+            File.WriteAllText(fileName, yaml);
+        }
+    }
 }
