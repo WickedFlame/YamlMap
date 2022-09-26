ba734c0bdaf30d7add585c413bebb0765062a578
Christian Walpen <christianwalpen@opacc.ch>
2022-09-26 21:54:16 +0200

    Added benchmarks

---
+++ src/Tests/YamlMap.Benchmark/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MeasureMap;
+using System.Text;
+using YamlMap;
+using YamlMap.Benchmark;
+
+var value = new StringBuilder().AppendLine("Simple: root")
+    .AppendLine("StringList:")
+    .AppendLine("  - one")
+    .AppendLine("  - 2")
+    .AppendLine("ObjList:")
+    .AppendLine("  - Simple: simple")
+    .AppendLine("  - Child:")
+    .AppendLine("      Simple: child").ToString();
+
+
+ProfilerSession.StartSession()
+    .SetIterations(10)
+    .SetThreads(10)
+    .Task(c =>
+    {
+        var reader = new YamlReader();
+        reader.Read<TestlItem>(value);
+    })
+    .RunSession()
+    .Trace();
+
+
+//ProfilerSession.StartSession()
+//    .SetIterations(10)
+//    .Task(c =>
+//    {
+//        var reader = new YamlReader();
+//        reader.Read<TestlItem>(value);
+//    })
+//    .RunSession()
+//    .Trace();
